aae3e18e4e49634d8f8838c16d2e7fa7c55c02bb
CASY82 <repaid11010@gmail.com>
2021-08-06 03:47:27 +0900

    Python Algorithm Test

--- Algo/etc/ICT_Implement_4-1.py
+++ Algo/etc/ICT_Implement_4-1.py
@@ -1,2 +1,26 @@
 #상하좌우
 
+n = int(input())
+move_plan = list(input().split())
+
+move = ['L', 'R', 'U', 'D']
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+x = 1
+y = 1
+
+for a in move_plan:
+    for i in range(len(move)):
+        if a == move[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+    if nx > n or nx < 1 or ny > n or nx < 1:
+        continue
+
+    x, y = nx, ny
+
+
+print(x, y)
+
