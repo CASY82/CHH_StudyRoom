798c9bdb42aa6b310dc1774972ad29f4aaa6c377
CASY82 <repaid11010@gmail.com>
2021-08-06 02:52:15 +0900

    Python Algorithm Test

--- Algo/Greedy/ICT_Greedy_3-3.py
+++ Algo/Greedy/ICT_Greedy_3-3.py
@@ -1,2 +1,35 @@
 #숫자 카드 게임
 
+#min을 이용한 풀이
+
+n, m = map(int, input().split())
+
+result = 0
+
+for i in range(n):
+    data = list(map(int, input().split()))
+
+    min_value = min(data)
+
+    result = max(result, min_value)
+
+print(result)
+
+#다른 풀이
+# n, m = map(int, input().split())
+#
+# result = 0
+#
+# for i in range(n):
+#     data = list(map(int, input().split()))
+#
+#     min_value = 10001
+#
+#     for a in data:
+#         min_value = min(min_value, a)
+#
+#     result = max(result, min_value)
+#
+# print(result)
+
+
