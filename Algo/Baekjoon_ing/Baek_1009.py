8dda5ad04b1cdca0cc650622f47059d478b44fb2
CASY82 <repaid11010@gmail.com>
2021-08-08 02:15:01 +0900

    Python Algorithm Test

--- Algo/Baekjoon_ing/Baek_1009.py
+++ Algo/Baekjoon_ing/Baek_1009.py
@@ -1,48 +1,54 @@
 #시간이 초과합니다.(pypy3로 하면 통과;;;)
 
-T = int(input())
-num_list = []
-result = [0 for _ in range(T)]
-data = 0
-
-for i in range(T):
-    a, b = map(int, input().split())
-
-    data = 1
-
-    for _ in range(b):
-        data = (data * a) % 10
-
-    if data == 0:
-        data = 10
-
-    result[i] = data
-
-for i in range(T):
-    print(result[i])
-
-
-#왜 틀리는 걸까?(반례를 못찾음...)
 # T = int(input())
-# result = []
-# d = 0
+# num_list = []
+# result = [0 for _ in range(T)]
+# data = 0
 #
 # for i in range(T):
 #     a, b = map(int, input().split())
 #
-#     d = a % 10
+#     data = 1
 #
-#     if d == 1 or d == 5 or d == 6:
-#         result.append(a % 10)
-#     elif d == 2 or d == 3 or d == 7 or d == 8:
-#         quota = b % 4
-#         result.append((a ** quota) % 10)
-#     elif d == 4 or d == 9:
-#         quota = b % 2
-#         result.append((a ** quota) % 10)
-#     else:
-#         result.append(10)
+#     for _ in range(b):
+#         data = (data * a) % 10
 #
+#     if data == 0:
+#         data = 10
 #
-# for i in range(len(result)):
-#      print(result[i])
+#     result[i] = data
+#
+# for i in range(T):
+#     print(result[i])
+
+
+#드디어 완료!
+T = int(input())
+result = []
+d = 0
+
+for i in range(T):
+    a, b = map(int, input().split())
+
+    d = a % 10
+
+    if d == 1 or d == 5 or d == 6:
+        result.append(a % 10)
+    elif d == 2 or d == 3 or d == 7 or d == 8:
+        quota = b % 4
+        if quota == 0:
+            result.append((a ** 4) % 10)
+        else:
+            result.append((a ** quota) % 10)
+    elif d == 4 or d == 9:
+        quota = b % 2
+        if quota == 0:
+            result.append((a ** 2) % 10)
+        else:
+            result.append((a ** quota) % 10)
+    else:
+        result.append(10)
+
+
+for i in range(len(result)):
+     print(result[i])
---
+++ Algo/Baekjoon_ing/Baek_1032.py
@@ -0,0 +1,19 @@
+N = int(input())
+word_list = []
+
+for i in range(N):
+    words = input()
+    word_list.append(words)
+
+def solution(N, word_list):
+    for i in range(N):
+        word_list[]
+
+
+
+
+
+
+
+
+print(solution(word_list))
