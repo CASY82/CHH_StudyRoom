433501febf4262f9f2e77ac072c3817c3494405c
CASY82 <repaid11010@gmail.com>
2021-08-08 02:54:41 +0900

    Python Algorithm Test

--- Algo/Baekjoon_ing/Baek_1032.py
+++ Algo/Baekjoon_ing/Baek_1032.py
@@ -1,3 +1,4 @@
+#드디어 거의 혼자 풀 수 있게 되었는가 한번에 맞추었다!
 N = int(input())
 word_list = []
 
@@ -5,15 +6,16 @@ for i in range(N):
     words = input()
     word_list.append(words)
 
-def solution(N, word_list):
-    for i in range(N):
-        word_list[]
+result_word = list(word_list[0])
+
+for i in range(1, len(word_list)):
+    compare_word = list(word_list[i])
+
+    for j in range(len(result_word)):
+        if result_word[j] == compare_word[j]:
+            continue
+        else:
+            result_word[j] = "?"
 
 
-
-
-
-
-
-
-print(solution(word_list))
+print("".join(result_word))
