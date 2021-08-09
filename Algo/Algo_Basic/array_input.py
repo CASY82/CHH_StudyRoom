2da5ba4411d53475aaa98dcf506af5b595a7e312
CASY82 <repaid11010@gmail.com>
2021-08-10 01:40:33 +0900

    Python Algorithm Test

--- Algo/Algo_Basic/array_input.py
+++ Algo/Algo_Basic/array_input.py
@@ -1,12 +1,15 @@
 #2차원 배열 입력 받기
 
-#1. arr[i] 하나씩 입력 받기
+# 1. arr[i] 하나씩 입력 받기
 # for i in range(B):
 # 	arr[i] = list(map(int, input().split()))
 
-#2. arr 한 list씩 append
+# 2. arr 한 list씩 append
 # for i in range(B):
 # 	arr.append(list(map(int, input().split())))
 
 # 3. 선언과 동시에 입력 받기
 # arr = [list(map(int, input().split())) for _ in range(B)]
+
+# 띄어쓰기 없이 입력 받는 경우 .split()제거
+# chess_board = [list(map(str, input())) for _ in range(8)]
--- Algo/Baekjoon_ing/Baek_1100.py
+++ Algo/Baekjoon_ing/Baek_1100.py
@@ -1,3 +1,14 @@
-chess_board = [list(input().split()) for _ in range(8)]
+count = 0
+#chess_board = [['.' for _ in range(8)] for _ in range(8)]
+chess_board = [list(map(str, input())) for _ in range(8)]
 
 print(chess_board)
+
+for i in range(8):
+    for j in range(8):
+        if i % 2 == 0 and j % 2 == 0 and chess_board[i][j] == 'F':
+            count += 1
+        elif i % 2 == 1 and j % 2 == 1 and chess_board[i][j] == 'F':
+            count += 1
+
+print(count)
