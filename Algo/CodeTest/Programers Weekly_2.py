1d94f857b88c85f60d7a7ce83851126e523aa406
CASY82 <repaid11010@gmail.com>
2021-08-09 23:02:55 +0900

    Python Algorithm Test

--- Algo/CodeTest/Programers Weekly_2.py
+++ Algo/CodeTest/Programers Weekly_2.py
@@ -45,3 +45,90 @@ def solution(scores):
     return result
 
 print(solution(scores))
+
+
+#풀이 참고
+# from collections import Counter
+#
+# def solution(scores):
+#     def rank(score):
+#         if score >= 90:
+#             return 'A'
+#         elif score >= 80:
+#             return 'B'
+#         elif score >= 70:
+#             return 'C'
+#         elif score >= 50:
+#             return 'D'
+#         else:
+#             return 'F'
+#     answer = ''
+#     # transpose
+#     for i in range(len(scores)):
+#         for j in range(1, i+1):
+#             scores[i][i-j], scores[i-j][i] = scores[i-j][i], scores[i][i-j]
+#     avg = []
+#     for i, s in enumerate(scores):
+#         if (max(s) == s[i] or min(s) == s[i]) and Counter(s)[s[i]] == 1:
+#             avg.append((sum(s) - s[i])/(len(s)-1))
+#         else:
+#             avg.append(sum(s)/len(s))
+#
+#     rank = ''.join(list(map(rank, avg)))
+#     return rank
+#
+# from collections import Counter
+# def solution(scores):
+#     answer = ''
+#
+#     for idx, score in enumerate(list(map(list, zip(*scores)))):
+#         length = len(score)
+#         if Counter(score)[score[idx]] == 1 and (max(score) == score[idx] or min(score) == score[idx]):
+#             del score[idx]
+#             length -= 1
+#
+#         grade = sum(score) / length
+#
+#         if grade >= 90:
+#             answer += 'A'
+#         elif grade >= 80:
+#             answer += 'B'
+#         elif grade >= 70:
+#             answer += 'C'
+#         elif grade >= 50:
+#             answer += 'D'
+#         else:
+#             answer += 'F'
+#
+#     return answer
+#
+#
+# def solution(scores):
+#     import statistics
+#     def scoring(x):
+#         if x >= 90:
+#             return 'A'
+#         elif 80 <= x < 90:
+#             return 'B'
+#         elif 70 <= x < 80:
+#             return 'C'
+#         elif 50 <= x < 70:
+#             return 'D'
+#         else:
+#             return 'F'
+#
+#     scores = list(map(list, zip(*scores)))
+#     students = len(scores)
+#     answer = ''
+#
+#     for i in range(students):
+#         maximum = max(scores[i])
+#         minimum = min(scores[i])
+#         if scores[i].count(maximum) == 1 and scores[i][i] == maximum:
+#             answer += scoring((sum(scores[i])-scores[i][i])/(students-1))
+#         elif scores[i].count(minimum) == 1 and scores[i][i] == minimum:
+#             answer += scoring((sum(scores[i])-scores[i][i])/(students-1))
+#         else:
+#             answer += scoring(statistics.mean(scores[i]))
+#
+#     return answer
