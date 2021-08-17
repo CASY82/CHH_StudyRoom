a42b1bb6eb32a855c5d5f65798f6ee30ff9e1ad2
CASY82 <repaid11010@gmail.com>
2021-08-18 04:51:58 +0900

    Python Algorithm Test

--- Algo/CodeTest/Programers Weekly_3.py
+++ Algo/CodeTest/Programers Weekly_3.py
@@ -1,13 +1,19 @@
+#중단 너무 어렵다. 아직은 내가 풀 수 있는 레벨은 아닌것 같으며 문제 풀이가 올라오는 경우 참고하는 걸로 하도록 하겠다.
+
 game_board = [[1,1,0,0,1,0],[0,0,1,0,1,0],[0,1,1,0,0,1],[1,1,0,1,1,1],[1,0,0,0,1,0],[0,1,1,1,0,0]]
 table = [[1,0,0,1,1,0],[1,0,1,0,1,0],[0,1,1,0,1,1],[0,0,1,0,0,0],[1,1,0,1,1,0],[0,1,0,0,0,0]]
 
 def solution(game_board, table):
-    result = -1
+    count = 0
 
+    for i in range(len(game_board)):
+        for j in range(len(game_board)):
+             if game_board[i][j] == 0:
+                game_board[i][j] = 1
+                count += 1
 
+    return count
 
-
-    return result
 
 
 print(solution(game_board, table))
