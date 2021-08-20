b1f00ad4734850ccfc11b7c2a9e110f0b845124b
CASY82 <repaid11010@gmail.com>
2021-08-20 22:33:48 +0900

    Python Algorithm Test

--- Algo/Algo_Basic/input.py
+++ Algo/Algo_Basic/input.py
@@ -3,3 +3,9 @@
 # list = list(map(int, input().split()))
 #
 # a = [int(x) for x in input().strip().split()]
+
+# 여러 줄 데이터 넣기
+# chess = []
+#
+# for i in range(n):
+#     chess.append(list(input()))
---
+++ Algo/etc/Baek_1018.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+chess = []
+
+for i in range(n):
+    chess.append(list(input()))
+
+
+print(chess)
+
