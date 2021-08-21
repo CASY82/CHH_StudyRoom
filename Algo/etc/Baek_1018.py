29b58da1c5c34a7dea99a1f7b746ec12cf139caf
CASY82 <repaid11010@gmail.com>
2021-08-21 22:04:09 +0900

    Python Algorithm Test

--- Algo/etc/Baek_1018.py
+++ Algo/etc/Baek_1018.py
@@ -1,9 +1,29 @@
-n, m = map(int, input().split())
-chess = []
+#결국 못품... 구현하는 능력이 너무나 떨어진다 해당 소스는 인터넷을 참고한 소스
 
-for i in range(n):
-    chess.append(list(input()))
+N, M = map(int, input().split())
+board = list()
+for i in range(N):
+    board.append(input())
+repair = list()
 
+for i in range(N-7):
+    for j in range(M-7):
+        first_W = 0
+        first_B = 0
+        for k in range(i,i+8):
+            for l in range(j,j + 8):
+                if (k + l) % 2 == 0:
+                    if board[k][l] != 'W':
+                        first_W = first_W+1
+                    if board[k][l] != 'B':
+                        first_B = first_B + 1
+                else:
+                    if board[k][l] != 'B':
+                        first_W = first_W+1
+                    if board[k][l] != 'W':
+                        first_B = first_B + 1
+        repair.append(first_W)
+        repair.append(first_B)
 
-print(chess)
+print(min(repair))
 
