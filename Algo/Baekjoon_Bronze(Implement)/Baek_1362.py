13987808645626716771b30f1852b0ea1303f56d
CASY82 <repaid11010@gmail.com>
2021-08-30 23:38:33 +0900

    Python Algorithm Test

--- Algo/Baekjoon_Bronze(Implement)/Baek_1362.py
+++ Algo/Baekjoon_Bronze(Implement)/Baek_1362.py
@@ -1,1 +1,29 @@
-#추후 풀것
+count = 0
+
+while True:
+    o, w = map(int, input().split())
+
+    if o == 0 and w == 0:
+        break
+
+    while True:
+        effect, num = map(str, input().split())
+
+        if effect == '#' and num == '0':
+            count += 1
+            break
+
+        if w > 0:
+            if effect == 'F':
+                w += int(num)
+            else:
+                w -= int(num)
+        else:
+            continue
+
+    if w > (o * 0.5) and w < (o * 2):
+        print(count, ':-)')
+    elif w <= 0:
+        print(count, 'RIP')
+    else:
+        print(count, ':-(')
