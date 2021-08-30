48685de198532092b643e4200eacd1cabf580dc3
CASY82 <repaid11010@gmail.com>
2021-08-31 02:49:22 +0900

    Python Algorithm Test

---
+++ Algo/etc/Baek_1004.py
@@ -0,0 +1,26 @@
+import math
+
+t = int(input())
+
+def two_point_len(x1, y1, x2, y2):
+    return math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))
+
+for i in range(t):
+    planet = []
+    count = 0
+    x1, y1, x2, y2 = map(int, input().split())
+    n = int(input())
+    for j in range(n):
+        planet.append(list(map(int, input().split())))
+
+    for k in range(len(planet)):
+        if two_point_len(x1, y1, planet[k][0], planet[k][1]) < planet[k][2] and two_point_len(x2, y2, planet[k][0], planet[k][1]) < planet[k][2]:
+            continue
+
+        if two_point_len(x1, y1, planet[k][0], planet[k][1]) < planet[k][2]:
+            count += 1
+
+        if two_point_len(x2, y2, planet[k][0], planet[k][1]) < planet[k][2]:
+            count += 1
+
+    print(count)
