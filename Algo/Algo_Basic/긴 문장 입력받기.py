cf96c11fe475d94b29effde259d7a8abd3bee39a
CASY82 <repaid11010@gmail.com>
2021-08-31 00:22:46 +0900

    Python Algorithm Test

--- Algo/Algo_Basic/긴 문장 입력받기.py
+++ Algo/Algo_Basic/긴 문장 입력받기.py
@@ -1,11 +1,11 @@
 # sys read를 이용하는 방법
-import sys
-line = sys.stdin.read()
-
-# #try except 이용하는 방법
-line = ''
-while True:
-	try:
-    	line = input()
-   	except EOFError:
-    	break
+# import sys
+# line = sys.stdin.read()
+#
+# # #try except 이용하는 방법
+# line = ''
+# while True:
+# 	try:
+#     	line = input()
+#    	except EOFError:
+#     	break
