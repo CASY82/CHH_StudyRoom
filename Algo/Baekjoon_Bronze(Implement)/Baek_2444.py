5f48982d33f6a153b1ceb4d506e12ac394ba7df5
CASY82 <repaid11010@gmail.com>
2021-09-18 04:41:11 +0900

    Python Algorithm Test

--- Algo/Baekjoon_Bronze(Implement)/Baek_2444.py
+++ Algo/Baekjoon_Bronze(Implement)/Baek_2444.py
@@ -14,3 +14,14 @@ for i in range(n*2-1):
                 print(' ', end='')
 
     print()
+
+
+#뭔가 너무 복잡하게 푼거 같다. 단순하게 푼 예시
+# n=int(input())
+#
+# for i in range(1,n+1):
+#   print(" "*(n-i)+"*"*(1+2*(i-1)))
+#
+#
+# for i in range(2,n+1):
+#   print(" "*(i-1)+"*"*(1+(n-i)*2))
