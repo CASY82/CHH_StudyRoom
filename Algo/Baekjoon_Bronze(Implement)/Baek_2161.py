4eac1f03ebddc1527dc6065e2177ea3ed50d5274
CASY82 <repaid11010@gmail.com>
2021-10-05 18:25:36 +0900

    Python Algorithm Test

--- Algo/Baekjoon_Bronze(Implement)/Baek_2161.py
+++ Algo/Baekjoon_Bronze(Implement)/Baek_2161.py
@@ -13,3 +13,16 @@ result.append(dq[0])
 
 for i in result:
     print(i, end=' ')
+
+
+# 다름 사람 풀이
+#
+# n=int(input())
+# card=list()
+# for i in range(1,n+1):
+#     card.append(i)
+# while len(card)>1:
+#     print(card.pop(0),end=' ')
+#     card.append(card[0])
+#     card.pop(0)
+# print(card[0])
