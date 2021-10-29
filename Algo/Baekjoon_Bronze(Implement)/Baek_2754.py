80d6a0462898070fb1bee297810ae542dfec042a
CASY82 <repaid11010@gmail.com>
2021-10-30 03:37:13 +0900

    Python Algorithm Test

---
+++ Algo/Baekjoon_Bronze(Implement)/Baek_2754.py
@@ -0,0 +1,28 @@
+c_lang = input()
+
+if c_lang == 'A+':
+    print(4.3)
+elif c_lang == 'A0':
+    print(4.0)
+elif c_lang == 'A-':
+    print(3.7)
+elif c_lang == 'B+':
+    print(3.3)
+elif c_lang == 'B0':
+    print(3.0)
+elif c_lang == 'B-':
+    print(2.7)
+elif c_lang == 'C+':
+    print(2.3)
+elif c_lang == 'C0':
+    print(2.0)
+elif c_lang == 'C-':
+    print(1.7)
+elif c_lang == 'D+':
+    print(1.3)
+elif c_lang == 'D0':
+    print(1.0)
+elif c_lang == 'D-':
+    print(0.7)
+else:
+    print(0.0)
