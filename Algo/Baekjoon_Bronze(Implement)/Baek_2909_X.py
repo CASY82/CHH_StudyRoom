eb4916bf446f534bad1cf50e8e05ac5d51a6e312
CASY82 <repaid11010@gmail.com>
2021-11-27 00:28:49 +0900

    Python Algorithm Test

--- Algo/Baekjoon_Bronze(Implement)/Baek_2909_X.py
+++ Algo/Baekjoon_Bronze(Implement)/Baek_2909_X.py
@@ -1,5 +1,6 @@
+# 도저히 반례를 모르겠다....
 # c, k = map(int, input().split())
-
+#
 # if len(str(c)) > k:
 #     print(round(c, -k))
 # elif len(str(c)) == k:
@@ -14,6 +15,7 @@
 # else:
 #     print(0)
 
+#현재 제출한 코드... 인터넷에서 긁어온 소스이다. 순수하게 값을 구하는 형태
 c, k = map(int, input().split())
 if k == 0:
     print(c)
@@ -25,3 +27,11 @@ else:
         print(n)
     else:
         print(n + t)
+
+# 내 코드와 가장 비슷하다고 보는데, 내가 해결하지 못한 반례가 해결된 모습인거 같다.
+# a, b = map(int, input().split())
+# if len(str(a)) >= b+1:
+#     print(round(a, -b))
+# else:
+#     if 5*10**(b-1) <= a: print(10**b)
+#     else: print(0)
