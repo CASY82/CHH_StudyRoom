52f01fa389469cd09963da50db30a28a282eb07d
CASY82 <repaid11010@gmail.com>
2021-11-29 17:35:31 +0900

    Python Algorithm Test

--- Algo/etc/Baek_4948.py
+++ Algo/etc/Baek_4948.py
@@ -25,6 +25,7 @@ while True:
 
     print(prime_count[n * 2] - prime_count[n])
 
+# 시간 초과
 # while True:
 #     n = int(input())
 #     prime = [True, True] + [False] * (2 * n)
@@ -46,3 +47,24 @@ while True:
 #             cnt += 1
 #
 #     print(cnt)
+
+#참고용 코드
+# import sys
+# input=sys.stdin.readline
+# def sol():
+#     n=123456*2
+#     prime=[True]*(n+1)
+#     prime[:2]=[False,False]
+
+#     에라토스테네스의 체를 굉장히 짧게 구현한것이 감명 깊었다.
+#     for i in range(2,int(n**0.5)+1):
+#         if prime[i]:
+#             prime[i*2::i]=[False]*(n//i-1)
+#     while True:
+#         nn=int(input())
+#         if nn==0:
+#             break
+#         나는 한번 더 직접 더하는 시간이 있어서 해당 코드보다 속도가 느렸으며 메모리가 2배로 사용되었으나 해당 코드는 prime 하나로 해결하였다.
+#         print(sum(1 for i in range(nn+1,2*nn+1) if prime[i]))
+#
+# sol()
