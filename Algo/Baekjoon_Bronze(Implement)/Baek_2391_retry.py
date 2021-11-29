af208e2569d51e7a9e18fd6a2bcc09258c2681ba
CASY82 <repaid11010@gmail.com>
2021-11-29 16:16:20 +0900

    Python Algorithm Test

--- Algo/Baekjoon_Bronze(Implement)/Baek_2391_retry.py
+++ Algo/Baekjoon_Bronze(Implement)/Baek_2391_retry.py
@@ -100,6 +100,8 @@ mommy
 iupc
 '''
 
+#포기;
+
 n = int(input())
 alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
