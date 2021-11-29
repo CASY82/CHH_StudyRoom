afe0367a5cec916240cf8a38d8727f7509ee4f04
CASY82 <repaid11010@gmail.com>
2021-11-29 23:04:45 +0900

    Python Algorithm Test

--- Algo/Baekjoon_Bronze(Implement)/Baek_13136.py
+++ Algo/Baekjoon_Bronze(Implement)/Baek_13136.py
@@ -9,3 +9,8 @@ else:
         print((r // n) * ((c // n) + 1))
     else:
         print(((r // n) + 1) * ((c // n) + 1))
+
+#참고한 소스;; 그냥 반올림하는 문제였다...
+# a, b, c = map(int, input().split())
+# import math
+# print(math.ceil(a/c) * math.ceil(b/c))
