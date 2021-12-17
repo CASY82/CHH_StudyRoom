ab6a72603416a2e9f15e559f552d2ef42fe1c2bc
CASY82 <repaid11010@gmail.com>
2021-12-17 09:57:20 +0900

    Python Algorithm Test

---
+++ Algo/Greedy/Programmers_큰 수 만들기_X.py
@@ -0,0 +1,25 @@
+number = "1924"
+k = 2
+
+def solution(number, k):
+    answer = ''
+    num_list = []
+
+    for i, v in enumerate(number):
+        while num_list and num_list[-1] < v and k > 0:
+            num_list.pop()
+            k -= 1
+
+        if k == 0:
+            num_list.extend([x for x in number[i:]])
+            break
+
+        num_list.append(v)
+
+    num_list = (num_list[:-k] if k > 0 else num_list)
+
+    answer = ''.join(num_list)
+
+    return answer
+
+print(solution(number, k))
