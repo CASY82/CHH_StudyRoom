921c9bc9e5cc5d62d31b76a042c9a42f719ea70d
CASY82 <repaid11010@gmail.com>
2021-12-21 00:03:46 +0900

    Python Algorithm Test

---
+++ Algo/etc/Baek_15656.py
@@ -0,0 +1,22 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+num = list(map(int, sys.stdin.readline().split()))
+
+num.sort()
+locate = [0] * (m+1)
+
+def backtrack(x):
+    if x == m + 1:
+        for i in range(1, m + 1):
+            print(locate[i], end=' ')
+        print()
+
+    else:
+        for j in range(1, n + 1):
+            locate[x] = num[j - 1]
+            backtrack(x + 1)
+            locate[x] = 0
+
+
+backtrack(1)
