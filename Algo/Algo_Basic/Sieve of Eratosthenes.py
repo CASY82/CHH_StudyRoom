c6e6e2baae7b16c290546d84e50ff527077a42ec
CASY82 <repaid11010@gmail.com>
2021-12-24 20:54:41 +0900

    Python Algorithm Test

--- Algo/Algo_Basic/Sieve of Eratosthenes.py
+++ Algo/Algo_Basic/Sieve of Eratosthenes.py
@@ -16,3 +16,33 @@ for i in range(2, int(math.sqrt(n)) + 1):
 for i in range(2, n + 1):
     if array[i]:
         print(i, end=' ')
+
+# 1번 방법이 훨씬 시간이 덜들고 빠르다.
+# numer = int(input())
+#
+# print("1번")
+# prime1 = [True] * (numer+1)
+# cnt = 0
+# for i in range(2, int(numer ** 0.5)+1):
+#     if prime1[i]:
+#         for j in range(i*i, numer+1, i):
+#             cnt += 1
+#             prime1[j] = False
+#
+# print(cnt)
+# print("2번")
+# prime = [True] * (numer+1)
+# cnt = 0
+#
+# for i in range(2, int(numer ** 0.5)+1):
+#     if prime[i]:
+#         j = 2
+#         while i * j <= numer:
+#             cnt += 1
+#             prime[i * j] = False
+#             j += 1
+# print(cnt)
+# if prime == prime1:
+#     print("똑같다")
+# else:
+#     print("다르다")
