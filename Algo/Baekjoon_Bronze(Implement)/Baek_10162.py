736b8e50887a347586e2a10f038f834a2b8bfa3e
CASY82 <repaid11010@gmail.com>
2021-12-27 10:23:58 +0900

    Python Algorithm Test

--- Algo/Baekjoon_Bronze(Implement)/Baek_10162.py
+++ Algo/Baekjoon_Bronze(Implement)/Baek_10162.py
@@ -24,3 +24,15 @@ else:
             t %= c
 
     print(result[0], result[1], result[2])
+
+# 좀 더 줄일 수 있었으나 안정적으로 위 코드와 같이 짜봤다. 아래는 참고
+# t = int(input())
+#
+# if t // 300 == 0 and t%10 == 0:
+#
+#     print(t//300, t//60, (t%60)//10)
+# elif t// 300 != 0 and t%10 == 0:
+#     y=(t%300)
+#     print(t//300, y//60, (y%60)//10)
+# else :
+#     print(-1)
