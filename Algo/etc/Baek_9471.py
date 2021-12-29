ac38c9115a5e1ae287b0b15543dcf30fe0d166b3
CASY82 <repaid11010@gmail.com>
2021-12-30 00:07:56 +0900

    Python Algorithm Test

--- Algo/etc/Baek_9471.py
+++ Algo/etc/Baek_9471.py
@@ -17,9 +17,30 @@ for _ in range(t):
                 result.pop()
                 break
 
-        #이부분만 아이디어를 좀 얻어서 풀었다.
+        #이 부분만 아이디어를 좀 얻어서 풀었다.
         mem[i] = (mem[i-1] + mem[i-2]) % m
         result.append(mem[i])
         i += 1
 
     print(case, len(result))
+
+# 내 코드를 줄이면 최종적으로 이 모양이 되지 않을까 해서 가져온 첨부 코드
+
+# import sys
+# input = sys.stdin.readline
+#
+# p = int(input())
+# answer = []
+#
+# def fibo (num):
+#     lst = [1,1]
+#     while True:
+#         lst.append((lst[-1]+lst[-2])%num)
+#         if lst[-1] == 0 and lst[-2] == 1:
+#             return len(lst)
+#
+# for _ in range(p):
+#     n,m = map(int,input().split())
+#     answer.append(f'{n} {fibo(m)}')
+#
+# print('\n'.join(answer))
