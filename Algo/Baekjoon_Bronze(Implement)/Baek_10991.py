3288fb38c17f199a9f3c1d688db3700577081141
CASY82 <repaid11010@gmail.com>
2022-01-04 10:39:04 +0900

    Python Algorithm Test

--- Algo/Baekjoon_Bronze(Implement)/Baek_10991.py
+++ Algo/Baekjoon_Bronze(Implement)/Baek_10991.py
@@ -27,3 +27,14 @@ for i in range(n):
             break
 
     print()
+
+# 내가 많이 멍청하긴 한갑다.. 위에 코드 생각하는데 너무 오래걸림;;; 그와중에 코드도 너무 길어서...
+# 아래는 참조 소스
+# num = int(input())
+#
+# for line in range(1, num+1):
+#     for _ in range(num - line):
+#         print(' ', end='')
+#     for _ in range(line):
+#         print('* ', end='')
+#     print()
