a4ad56d8023e53d078e40e9387e70ecaf82c466d
CASY82 <repaid11010@gmail.com>
2022-01-19 00:11:49 +0900

    Python Algorithm Test

--- Algo/Baekjoon_Bronze(Implement)/Baek_16561.py
+++ Algo/Baekjoon_Bronze(Implement)/Baek_16561.py
@@ -1,7 +1,7 @@
 import sys
 
 n = int(sys.stdin.readline())
-numList = []
+# numList = []
 
 cnt = 0
 
@@ -9,7 +9,8 @@ for i in range(3, n+1, 3):
     for j in range(3, n-i+1, 3):
         if n - i - j <= 0:
             continue
-        numList.append([i,j,n-i-j])
+        #이거 빼도되는데 ㅋ
+        # numList.append([i,j,n-i-j])
         cnt += 1
 
 print(cnt)
