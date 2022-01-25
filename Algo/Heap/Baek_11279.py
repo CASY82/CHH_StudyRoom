edad4ca8906e3003069f63bdb578ed7e837eb6d8
CASY82 <repaid11010@gmail.com>
2022-01-25 23:17:50 +0900

    Python Algorithm Test

---
+++ Algo/Heap/Baek_11279.py
@@ -0,0 +1,17 @@
+import sys
+import heapq
+
+n = int(sys.stdin.readline())
+heap = []
+
+for _ in range(n):
+    num = int(sys.stdin.readline())
+
+    if num == 0 and len(heap) == 0:
+        print(0)
+
+    elif num == 0:
+        print(-heapq.heappop(heap))
+
+    else:
+        heapq.heappush(heap, -num)
