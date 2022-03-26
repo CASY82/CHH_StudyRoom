efdd196e6e8a8a73dbe593ad57595f23c7ba0948
CASY82 <repaid11010@gmail.com>
2022-03-26 18:04:56 +0900

    Python Algorithm Test

---
+++ Algo/Baekjoon_Bronze(Implement)/Baeke_6376.py
@@ -0,0 +1,26 @@
+# def fac(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return fac(n-1) * n
+#
+# result = 0
+#
+# print("n e")
+# print("- -----------")
+# for i in range(10):
+#     result += 1 / fac(i)
+#     print(i, result)
+
+print("n e")
+print("- -----------")
+print("0 1")
+print("1 2")
+print("2 2.5")
+print("3 2.666666667")
+print("4 2.708333333")
+print("5 2.716666667")
+print("6 2.718055556")
+print("7 2.718253968")
+print("8 2.718278770")
+print("9 2.718281526")
