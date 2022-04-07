f1f37fa4df69987b578f723018be148cb0e95591
CASY82 <repaid11010@gmail.com>
2022-04-07 19:54:24 +0900

    Python Algorithm Test

---
+++ Algo/DFS_BFS/Baek_1197.py
@@ -0,0 +1,39 @@
+import sys
+
+def getParent(parent, x):
+    if parent[x] == x:
+        return x
+
+    parent[x] = getParent(parent, parent[x])
+    return parent[x]
+
+def unionParent(parent, a, b):
+    a = getParent(parent, a)
+    b = getParent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+v, e = map(int, sys.stdin.readline().split())
+parent = [i for i in range(v + 1)]
+
+edges = []
+total_cost = 0
+
+for _ in range(e):
+    a, b, cost = map(int, sys.stdin.readline().split())
+
+    edges.append((cost, a, b))
+
+edges.sort()
+
+for i in range(e):
+    cost, a, b = edges[i]
+
+    if getParent(parent, a) != getParent(parent, b):
+        unionParent(parent, a, b)
+        total_cost += cost
+
+print(total_cost)
