64870655340ccb35a5caf695a21f5a977fbe9f94
CASY82 <repaid11010@gmail.com>
2022-05-02 21:44:23 +0900

    Python Algorithm Test

---
+++ Algo/Dynamic_Programming/Baek_2294.py
@@ -0,0 +1,24 @@
+import sys
+
+INF = int(1e9)
+n, k = map(int, sys.stdin.readline().split())
+
+coin = []
+dp = [INF for _ in range(k+1)]
+
+for _ in range(n):
+    val = int(sys.stdin.readline())
+    coin.append(val)
+    if val < k:
+        dp[val] = 1
+
+for i in range(1, k+1):
+    for j in range(len(coin)):
+        if i - coin[j] > 0:
+            if dp[i] > dp[i - coin[j]] + 1:
+                dp[i] = dp[i - coin[j]] + 1
+
+if dp[-1] < INF:
+    print(dp[-1])
+else:
+    print(-1)
