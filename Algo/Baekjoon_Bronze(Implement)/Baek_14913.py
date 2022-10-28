eee00d4a00017e8ce2ad8a8d4103411707237ec7
CASY82 <repaid11010@gmail.com>
2022-10-28 22:01:56 +0900

    Python Algorithm Test

--- Algo/Baekjoon_Bronze(Implement)/Baek_14913.py
+++ Algo/Baekjoon_Bronze(Implement)/Baek_14913.py
@@ -5,4 +5,9 @@ a, d, k = map(int, sys.stdin.readline().split())
 if (k - a) % d != 0:
     print("X")
 else:
-    print(((k - a) // d) + 1)
+    result = ((k - a) // d) + 1
+
+    if result > 0:
+        print(result)
+    else:
+        print("X")
