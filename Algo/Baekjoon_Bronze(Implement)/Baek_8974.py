789c71a0bb4e9f7fbef1fce68065d0b2f17cf3c0
CASY82 <repaid11010@gmail.com>
2023-02-17 23:01:15 +0900

    Python Algorithm Test

--- Algo/Baekjoon_Bronze(Implement)/Baek_8974.py
+++ Algo/Baekjoon_Bronze(Implement)/Baek_8974.py
@@ -1,5 +1,6 @@
 import sys
 
+# 비효율적인것은 알고있으나, 이게 제일 쉬워보였다... ㅋㅋ
 a, b = map(int, sys.stdin.readline().split())
 result = 0
 
@@ -13,3 +14,18 @@ for num in range(1, 1001):
         cnt += 1
 
 print(sum(easy_arr[a-1:b]))
+
+# 다른 사람 풀이
+# a,b=map(int,input().split())
+# c = []
+# total = 0
+# total2 = 0
+# for i in range(1,b+1):
+#     for j in range(0,i):
+#         c.append(i)
+#         total += 1
+#     if total > b:
+#             break
+# for i in range(a-1,b):
+#     total2 += c[i]
+# print(total2)
