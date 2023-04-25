95d013832b825472bed347e8716927c7d5624305
CASY82 <repaid11010@gmail.com>
2023-04-25 20:07:23 +0900

    Python Algorithm Test

--- Algo/Baekjoon_Bronze(Implement)/Baek_5928.py
+++ Algo/Baekjoon_Bronze(Implement)/Baek_5928.py
@@ -12,3 +12,11 @@ if daysToMin < 0 and hoursToMin < 0 and minToMin < 0 or result < 0:
     print(-1)
 else:
     print(result)
+
+# 참고
+
+# D, H, M = map(int, input().split())
+# start = 11 * 24 * 60 + 11 * 60 + 11
+# end = D * 24 * 60 + H * 60 + M
+# result = end - start
+# print(result if result >= 0 else -1)
