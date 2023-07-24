0d6cc7364cd5c9234e64b67b1314d80d6bff4fcc
CASY82 <repaid11010@gmail.com>
2023-07-24 19:16:44 +0900

    Python Algorithm Test

--- Algo/Baekjoon_Bronze(Implement)/Baek_5342.py
+++ Algo/Baekjoon_Bronze(Implement)/Baek_5342.py
@@ -24,3 +24,14 @@ if result != 0:
     print("$" + str(result)[:-2] + "." + str(result)[-2] + str(result)[-1])
 else:
     print("$0.00")
+
+# 다른 사람 풀이 참고
+
+# aw={"Paper":57.99,"Printer":120.50,"Planners":31.25,"Binders":22.50,"Calendar":10.95,"Notebooks"	:11.20,"Ink":66.95}
+# su=0
+# while 1:
+#     try:
+#         su+=aw[input()]
+#     except:
+#         break
+# print(f"${su}")
