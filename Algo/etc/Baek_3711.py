4d45597d776240387f36d4c5f966401e2dbba940
CASY82 <repaid11010@gmail.com>
2023-08-10 22:36:17 +0900

    Python Algorithm Test

--- Algo/etc/Baek_3711.py
+++ Algo/etc/Baek_3711.py
@@ -24,3 +24,34 @@ for _ in range(n):
                 break
 
             i += 1
+
+# 다른 사람 풀이
+# import sys
+#
+# input = sys.stdin.readline
+#
+#
+# def solution(G, student_numbers):
+#     m = 1
+#     while True:
+#         remainder_set = set()
+#         for i in student_numbers:
+#             remainder = i % m
+#             if remainder in remainder_set:
+#                 break
+#             remainder_set.add(remainder)
+#         else:
+#             return m
+#         m += 1
+#
+#
+# N = int(input())
+# for i in range(N):
+#     G = int(input())
+#     student_numbers = []
+#     for j in range(G):
+#         nums = int(input())
+#         student_numbers.append(nums)
+#
+#     res = solution(G, student_numbers)
+#     print(res)
