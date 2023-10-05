0db3cb907572d233e57c8eae40b5018600e8b5b3
CASY82 <repaid11010@gmail.com>
2023-10-05 23:01:24 +0900

    Python Algorithm Test

---
+++ Algo/Baekjoon_Bronze(Implement)/Baek_28691.py
@@ -0,0 +1,13 @@
+import sys
+
+club_pick = sys.stdin.readline().strip()
+
+club = {
+    "M" : "MatKor",
+    "W" : "WiCys",
+    "C" : "CyKor",
+    "A" : "AlKor",
+    "$" : "$clear"
+}
+
+print(club[club_pick])
