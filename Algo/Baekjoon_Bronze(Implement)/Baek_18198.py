05658246605d9cad2a7029e6b8331d5e5f9f14ee
CASY82 <repaid11010@gmail.com>
2023-10-13 00:07:15 +0900

    Python Algorithm Test

--- Algo/Baekjoon_Bronze(Implement)/Baek_18198.py
+++ Algo/Baekjoon_Bronze(Implement)/Baek_18198.py
@@ -29,3 +29,29 @@ for i in range(0, len(play), 2):
             else:
                 print("B")
             break
+
+# 다른 사람 풀이
+# score = input()
+# score_a = []
+# score_b = []
+#
+# cnt = 0
+# for i in range(int(len(score) / 2)):
+#     if score[cnt] == 'A':
+#         score_a.append(score[cnt:cnt + 2])
+#         cnt += 2
+#     else:
+#         score_b.append(score[cnt:cnt + 2])
+#         cnt += 2
+#
+# A = 0
+# B = 0
+# for j in score_a:
+#     A += int(j[-1])
+# for k in score_b:
+#     B += int(k[-1])
+#
+# if max(A, B) == A:
+#     print('A')
+# else:
+#     print('B')
