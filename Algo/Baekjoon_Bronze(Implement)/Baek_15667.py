d55d57157dfc5cb691fe6be655adead2e8c798bd
CASY82 <repaid11010@gmail.com>
2023-11-06 20:42:43 +0900

    Python Algorithm Test

--- Algo/Baekjoon_Bronze(Implement)/Baek_15667.py
+++ Algo/Baekjoon_Bronze(Implement)/Baek_15667.py
@@ -8,3 +8,11 @@ result_plus = ((-1) + math.sqrt(tmp)) // 2
 result_minus = ((-1) - math.sqrt(tmp)) // 2
 
 print(max(int(result_plus), int(result_minus)))
+
+# 다른 사람 풀이
+# n = int(input())
+#
+# for i in range(1, n + 1):
+#     if i**2 + i + 1 - n == 0:
+#         break
+# print(i)
