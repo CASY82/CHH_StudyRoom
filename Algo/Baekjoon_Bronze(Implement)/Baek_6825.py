61568910a3b7ac6052fc6a484b75e603cb0bce70
CASY82 <repaid11010@gmail.com>
2023-11-27 23:13:16 +0900

    Python Algorithm Test

---
+++ Algo/Baekjoon_Bronze(Implement)/Baek_6825.py
@@ -0,0 +1,13 @@
+import sys
+
+weight = float(sys.stdin.readline())
+height = float(sys.stdin.readline())
+
+bmi = weight / (height * height)
+
+if bmi > 25:
+    print("Overweight")
+elif 18.5 <= bmi <= 25:
+    print("Normal weight")
+else:
+    print("Underweight")
