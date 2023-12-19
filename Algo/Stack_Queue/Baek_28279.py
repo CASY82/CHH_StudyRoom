31b81007684cf1d953d2059013d392af0a2f4a0f
CASY82 <repaid11010@gmail.com>
2023-12-19 23:49:42 +0900

    Python Algorithm Test

---
+++ Algo/Stack_Queue/Baek_28279.py
@@ -0,0 +1,48 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+
+deq = deque()
+
+for _ in range(n):
+    command = list(sys.stdin.readline().strip().split())
+
+    if command[0] == '1':
+        deq.appendleft(command[1])
+
+    if command[0] == '2':
+        deq.append(command[1])
+
+    if command[0] == '3':
+        if deq:
+            print(deq.popleft())
+        else:
+            print(-1)
+
+    if command[0] == '4':
+        if deq:
+            print(deq.pop())
+        else:
+            print(-1)
+
+    if command[0] == '5':
+        print(len(deq))
+
+    if command[0] == '6':
+        if deq:
+            print(0)
+        else:
+            print(1)
+
+    if command[0] == '7':
+        if deq:
+            print(deq[0])
+        else:
+            print(-1)
+
+    if command[0] == '8':
+        if deq:
+            print(deq[-1])
+        else:
+            print(-1)
