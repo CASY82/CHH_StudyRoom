7ef88478c9fef0532cdf1b811774db82c20f8181
CASY82 <repaid11010@gmail.com>
2024-06-12 01:40:40 +0900

    Python Algorithm Test

---
+++ Algo/Baekjoon_Bronze(Implement)/Baek_16600.py
@@ -0,0 +1,26 @@
+import math
+import sys
+
+n = int(sys.stdin.readline())
+
+print("{:.8f}".format(math.sqrt(n) * 4))
+
+# 다른 사람 풀이
+
+# import sys
+#
+#
+# def main() -> None:
+#     input = sys.stdin.readline
+#
+#     x = int(input())
+#     y = (x**0.5) * 4
+#     print(f"{y:.14f}")
+#
+#
+# sys.exit(main())
+
+# 다른 사람 풀이 2
+# import math
+# N = int(input())
+# print(math.sqrt(N) * 4)
