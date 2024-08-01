1b75071e4a300375578b8d25a751f0ceb0e3f2f8
CASY82 <repaid11010@gmail.com>
2024-08-01 19:55:27 +0900

    Python Algorithm Test

---
+++ Algo/Baekjoon_Bronze(Implement)/Baek_24724.py
@@ -0,0 +1,13 @@
+import sys
+
+t = int(sys.stdin.readline())
+
+for i in range(t):
+    n = int(sys.stdin.readline())
+    a, b = map(int, sys.stdin.readline().split())
+
+    for _ in range(n):
+        ui, vi = map(int, sys.stdin.readline().split())
+
+    print("Material Management {}".format(i + 1))
+    print("Classification ---- End!")
