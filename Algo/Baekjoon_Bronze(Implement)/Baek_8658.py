040a9b597acb59df1cbcaec8490328eff2a9ca99
CASY82 <repaid11010@gmail.com>
2024-10-08 19:45:33 +0900

    Python Algorithm Test

--- Algo/Baekjoon_Bronze(Implement)/Baek_8658.py
+++ Algo/Baekjoon_Bronze(Implement)/Baek_8658.py
@@ -11,3 +11,14 @@ for i in range(1, n):
         break
 
 print(min_num, max_num)
+
+# 다른 사람 풀이
+# n = int(input())
+# for i in range(2, n):
+#     if n % i != 0:
+#         print(i,end=' ')
+#         break
+# for i in range(n-1, 1, -1):
+#     if n % i != 0:
+#         print(i,end=' ')
+#         break
