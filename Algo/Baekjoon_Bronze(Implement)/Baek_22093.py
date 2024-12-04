514b0fa1594aaa2d03bbae0b063c1429d2eef089
CASY82 <repaid11010@gmail.com>
2024-12-04 20:16:26 +0900

    Python Algorithm Test

--- Algo/Baekjoon_Bronze(Implement)/Baek_22093.py
+++ Algo/Baekjoon_Bronze(Implement)/Baek_22093.py
@@ -5,8 +5,6 @@ t = int(sys.stdin.readline())
 for _ in range(t):
     n, a, b = map(int, sys.stdin.readline().split())
 
-    do_not_solve = n - a
-
     low = max(a - b, 0)
     high = min(n - b, a)
 
