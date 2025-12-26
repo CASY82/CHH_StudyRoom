34529ae8cf065ff8dd8074d60b137757b987689f
CASY82 <repaid11010@gmail.com>
2025-12-27 01:20:10 +0900

    Python Algorithm Test

--- Algo/Baekjoon_Bronze(Implement)/Baek_26575.py
+++ Algo/Baekjoon_Bronze(Implement)/Baek_26575.py
@@ -1,11 +1,14 @@
 import sys
+from decimal import Decimal, ROUND_HALF_UP
 
 n = int(sys.stdin.readline())
 
 for _ in range(n):
-    d, f, p = map(float, sys.stdin.readline().split())
+    d, f, p = sys.stdin.readline().split()
 
-    d = int(d)
+    d = Decimal(d)
+    f = Decimal(f)
+    p = Decimal(p)
 
-    total = d * f * p
-    print(f"${total:.2f}")
+    total = (d * f * p).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
+    print(f"${total}")
