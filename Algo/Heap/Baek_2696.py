367a2d2c952c8dbb0217d4d959b370bd66860b9c
CASY82 <repaid11010@gmail.com>
2026-03-29 21:17:10 +0900

    Python Algorithm Test

---
+++ Algo/Heap/Baek_2696.py
@@ -0,0 +1,31 @@
+# 중앙값 구하기
+import sys
+import heapq
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    m = int(sys.stdin.readline())
+    nums = list(map(int, sys.stdin.readline().split()))
+
+    left = [] # 최대 힙
+    right = [] # 최소 힙
+    res = []
+
+    for i, num in enumerate(nums):
+        if not left or num <= -left[0]:
+            heapq.heappush(left, -num)
+        else:
+            heapq.heappush(right, num)
+
+        if len(left) > len(right) + 1:
+            heapq.heappush(right, -heapq.heappop(left))
+        elif len(right) > len(left):
+            heapq.heappush(left, -heapq.heappop(right))
+
+        if i % 2 == 0:
+            res.append(-left[0])
+
+    print(len(res))
+    for i in range(0, len(res), 10):
+        print(*res[i:i + 10])
