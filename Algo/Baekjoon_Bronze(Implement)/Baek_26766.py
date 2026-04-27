bf9df8db0397c7d9689b152ed18defecaa039372
CASY82 <repaid11010@gmail.com>
2026-04-28 02:22:05 +0900

    Python Algorithm Test

---
+++ Algo/Baekjoon_Bronze(Implement)/Baek_26766.py
@@ -0,0 +1,16 @@
+# Serca
+
+import sys
+
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    print(" @@@   @@@ ")
+    print("@   @ @   @")
+    print("@    @    @")
+    print("@         @")
+    print(" @       @ ")
+    print("  @     @  ")
+    print("   @   @   ")
+    print("    @ @    ")
+    print("     @     ")
