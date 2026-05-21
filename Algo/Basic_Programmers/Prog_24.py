6893864ea5fac8411bac18648563cea2f7246af8
CASY82 <repaid11010@gmail.com>
2026-05-21 22:00:11 +0900

    Python Algorithm Test

---
+++ Algo/Basic_Programmers/Prog_24.py
@@ -0,0 +1,19 @@
+# 덧칠하기
+
+def solution(n, slicer, num_list):
+    answer = []
+
+    if n == 1:
+        return num_list[:slicer[1] + 1]
+    elif n == 2:
+        return num_list[slicer[0]:]
+    elif n == 3:
+        return num_list[slicer[0]:slicer[1] + 1]
+    elif n == 4:
+        for i in range(slicer[0], slicer[1] + 1, slicer[2]):
+            answer.append(num_list[i])
+
+        return answer
+
+print(solution(3, [1, 5, 2], [1, 2, 3, 4, 5, 6, 7, 8, 9]))
+print(solution(4, [1, 5, 2], [1, 2, 3, 4, 5, 6, 7, 8, 9]))
