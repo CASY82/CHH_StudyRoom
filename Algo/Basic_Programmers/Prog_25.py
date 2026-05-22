ddaa75cc7763fbea562fc95549f786da0ced592f
CASY82 <repaid11010@gmail.com>
2026-05-22 22:39:37 +0900

    Python Algorithm Test

---
+++ Algo/Basic_Programmers/Prog_25.py
@@ -0,0 +1,13 @@
+# qr code
+
+def solution(q, r, code):
+    answer = ''
+
+    for i in range(len(code)):
+        if i % q == r:
+            answer = answer + code[i]
+
+    return answer
+
+print(solution(3, 1, "qjnwezgrpirldywt"))
+print(solution(1, 0, "programmers"))
