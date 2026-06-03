527f788dcc095f3468241568e724c83b71a639f4
CASY82 <repaid11010@gmail.com>
2026-06-03 21:59:25 +0900

    Python Algorithm Test

---
+++ Algo/Basic_Programmers/Prog_36.py
@@ -0,0 +1,16 @@
+# 수 조작하기 1
+
+def solution(n, control):
+    for i in range(len(control)):
+        if control[i] == "w":
+            n += 1
+        elif control[i] == "s":
+            n -= 1
+        elif control[i] == "d":
+            n += 10
+        else:
+            n -= 10
+
+    return n
+
+print(solution(0, "wsdawsdassw"))
