711307627e1d63ca620ed6fd07f7c5e6e4e41f44
CASY82 <repaid11010@gmail.com>
2026-07-12 19:34:53 +0900

    Python Algorithm Test

---
+++ Algo/Basic_Programmers/Prog_58.py
@@ -0,0 +1,13 @@
+# flag에 따라 다른 값 반환하기
+
+def solution(i, j, k):
+    tmp = ""
+
+    for num in range(i, j + 1):
+        tmp = tmp + str(num)
+
+    return tmp.count(str(k))
+
+print(solution(1, 13, 1))
+print(solution(10, 50, 5))
+print(solution(3, 10, 2))
