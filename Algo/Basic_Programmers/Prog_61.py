26ef4fd2df50909bfb7b109bb8e240962620060f
CASY82 <repaid11010@gmail.com>
2026-07-19 18:44:10 +0900

    Python Algorithm Test

---
+++ Algo/Basic_Programmers/Prog_61.py
@@ -0,0 +1,11 @@
+# 문자열 섞기
+
+def solution(str1, str2):
+    answer = ''
+
+    for i in range(len(str1)):
+        answer = answer + str1[i] + str2[i]
+
+    return answer
+
+print(solution("aaaaa", "bbbbb"))
